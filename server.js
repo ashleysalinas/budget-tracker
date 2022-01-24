2b95335ba98c96983f5f315618eaca8ade3ef1e3
ashleysalinas <ashleysalinas98@protonmail.com>
2022-01-24 11:55:23 -0600

    env for protection

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 const PORT = 3000;
-
+//const MONGODB_URI = "mongodb+srv://ashleysalinas:root@Cluster0.ja3k3.mongodb.net/Cluster0?retryWrites=true&w=majority"
 const app = express();
-
+require('dotenv').config()
 app.use(logger("dev"));
 
 app.use(compression());
@@ -14,7 +14,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useFindAndModify: false
 });
