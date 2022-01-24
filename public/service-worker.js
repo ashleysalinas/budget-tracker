84f83a5f334132d8fab7e083a52069321b98199e
ashleysalinas <ashleysalinas98@protonmail.com>
2022-01-24 11:37:23 -0600

    indexeddb

--- public/service-worker.js
+++ public/service-worker.js
@@ -41,12 +41,33 @@ self.addEventListener('activate', function (e) {
 
 self.addEventListener('fetch', function (e) {
   console.log('fetch request : ' + e.request.url);
+  if (e.request.url.includes("/api/")) {
+    e.respondWith(
+      caches.open(DATA_CACHE_NAME).then(cache => {
+        return fetch(e.request)
+          .then(response => {
+            // If the response was good, clone it and store it in the cache.
+            if (response.status === 200) {
+              cache.put(e.request.url, response.clone());
+            }
+
+            return response;
+          })
+          .catch(err => {
+            // Network request failed, try to get it from the cache.
+            return cache.match(e.request);
+          });
+      }).catch(err => console.log(err))
+    );
+
+    return;
+  }
   e.respondWith(
       caches.match(e.request).then(function (request) {
           if (request) { 
               console.log('responding with cache : ' + e.request.url);
               return request
-          } else {       // if there are no cache
+          } else {       // if there is no cache
               console.log('file is not cached, fetching : ' + e.request.url);
               return fetch(e.request)
           }
